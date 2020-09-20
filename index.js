97bd9ec46f2aacd4bd4096a918aaf79fc948af2a
BuildTools <unconfigured@null.spigotmc.org>
2020-09-20 17:06:50 -0300

    new file:   TODO.txt 	modified:   adlibs/ayaya.mp3 	new file:   adlibs/hahanice.mp3 	new file:   adlibs/kevinio.mp3 	new file:   adlibs/myqueen.mp3 	new file:   adlibs/scream1.mp3 	new file:   adlibs/slap.mp3 	new file:   adlibs/wotah.mp3 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json
    Added New Audio Files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //TODO: Seperate Sound playing into a module to keep track of states
-//
+//Try statement to catch errors if API server isn't running
 
 const tmi = require ('tmi.js');
 const fs = require('fs');
@@ -17,7 +17,7 @@ const options = {
         username: 'FluteBotXD',
         password: 'oauth:g2umjwjbvg0r9hk1hf1emu0moqjoz7'
     },
-    channels: ['Forecasterxd']
+    channels: ['ishikexd']
 };
 
 //Create client with options
@@ -89,7 +89,10 @@ function onMessageHandler (target, tags, msg, self)
                             client.say(target, `@${tags["display-name"]}, sounds unmuted`);
                         }
                         return;
-                    
+                    //figure out how to do !so
+                    /*case '!so':
+                        client.say(target, `Check out ${1}, they are playing ${game ${1}} at https://twitch.tv/$%7B1%7D`);
+                        break;*/
                     //Disconnect Bot
                     case '!leave':
                         client.action('Forecasterxd', `FluteBotXD Disconnected!`);
@@ -112,8 +115,6 @@ function onMessageHandler (target, tags, msg, self)
             case '!hello':
                 client.say(target, `Hello @${tags["display-name"]}!`);
                 break;
-            
-            
 
             //list of Sounds
             case '!sounds':
@@ -125,15 +126,55 @@ function onMessageHandler (target, tags, msg, self)
                 client.say(target, `@${tags["display-name"]} List of sounds: ${response}`);
                 break;
             
-            case '!badges':
-                client.say(target, `${tags["badges"]}, sounds unmuted`);
+            case '!twitter':
+                client.say(target, `Twitter: https://twitter.com/ishikexd`);
                 break;
-            //
+            
+            case '!instagram':
+                client.say(target, `Instagram: https://www.instagram.com/ishikexd`);
+                break;
+            
+            case '!spotify':
+                client.say(target, `Spotify: ISHIKE ---> https://open.spotify.com/artist/6rkTsOVWp7JUcGlWKEvQFY?nd=1`);
+                break;
+
+            case '!soundcloud':
+                client.say(target, `SoundCloud: https://soundcloud.com/ishikexd`);
+                break;
+
+            case '!youtube':
+                client.say(target, `Youtube: https://www.youtube.com/channel/UCGfjy6De8oP1-unmgxnhCCA`);
+                break;
+
+            case '!discord':
+                client.say(target, `Check out our discord here ---> https://discord.gg/q8aveG2`);
+                break;
+            
+            
+
+            case '!randomshit':
+                client.say(target, `Community music time! Send in your own created sound samples to help assemble a brand new track with Kevin! :) Send them to Kevin via a discord DM. (MP3)`);
+                break;
+
+            case '!socials':
+                client.say(target, `Find Kevin On The Web!: Twitter: https://twitter.com/IshikeXD ------------ 
+                                    Insta: https://www.instagram.com/ishikexd/?hl=en --- 
+                                    Soundcloud: https://soundcloud.com/ishikexd --- 
+                                    Discord: https://discord.gg/q8aveG2`);
+                break;
+
+            //figure out how to do !fck
+            /*case '!fck':
+                client.say(target, ``);
+                break;*/
+
+                
+            //Get people to use !commands
             case '!help':
                 client.say(target, `${tags["display-name"]}, use !commands for a list of commands`);
                 break;
 
-            //
+            //List of Commands
             case '!commands':
                 var commands = ['!hello', '!leave', '!sounds'];
                 var response = ''
@@ -189,8 +230,6 @@ function checkSounds(commandName)
     let sounds = fs.readdirSync('adlibs');
     for (i = 0; i < soundfiles.length; i++)
     {
-        //console.log(soundfiles[i])
-        //console.log('Command:' + commandName.substring(1));
         if (commandName.substring(1) == soundfiles[i])
         {
             console.log(`Sound: ${commandName} found`)
